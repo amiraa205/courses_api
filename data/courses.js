9f794283b04188952b73d778d99c3e562b2cc9e3
Amira Hussein <124730533+amiraa205@users.noreply.github.com>
2024-09-07 11:07:25 +0300

    Add files via upload

---
+++ data/courses.js
@@ -0,0 +1,18 @@
+
+let courses = [
+    { 
+        id: 1,
+        title: "jscourse",
+        price: 100
+    },
+    {
+        id: 2,
+        title: "Reactcourse",
+        price: 400
+    }
+]
+
+
+module.exports = {
+    courses
+}
